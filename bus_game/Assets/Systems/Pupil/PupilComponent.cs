2e04ff0eddc0e75259003ea6335db1623c4c5172
Emil Bohleber <me@limered.de>
2023-10-01 15:03:25 +0200

    add pupil syestem and prefab

---
+++ bus_game/Assets/Systems/Pupil/PupilComponent.cs
@@ -0,0 +1,11 @@
+﻿using SystemBase.Core.Components;
+using UnityEngine;
+
+namespace Systems.Pupil
+{
+    public class PupilComponent : GameComponent
+    {
+        public PupilState State { get; set; } = PupilState.Outside;
+        public GameObject CurrentTarget { get; set; }
+    }
+}
---
+++ bus_game/Assets/Systems/Pupil/PupilSystem.cs
@@ -0,0 +1,24 @@
+﻿using SystemBase.Core.GameSystems;
+
+namespace Systems.Pupil
+{
+    [GameSystem]
+    public class PupilSystem : GameSystem<PupilComponent>
+    {
+        public override void Register(PupilComponent component)
+        {
+            
+        }
+        
+        private bool TargetReached(PupilComponent component)
+        {
+            return (component.CurrentTarget.transform.position - component.transform.position).magnitude < 0.1f;
+        }
+    }
+
+    public enum PupilState
+    {
+        Outside,
+        Inside
+    }
+}
